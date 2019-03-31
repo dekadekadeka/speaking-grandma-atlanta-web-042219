cdc2804304fbde5cb5d4ea3e3e2afe5e81920ce9
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-31 22:23:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif !phrase.upcase
+  elsif phrase.upcase == false
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
